90abffdcb457e76e7054605c0863084976408dc2
Gabor Horvath <dev.gaborhorvath@gmail.com>
2018-03-18 22:29:48 +0100

    Post and display the chat message

--- app/src/main/java/com/ghataa/firebasechatapp/MainActivity.java
+++ app/src/main/java/com/ghataa/firebasechatapp/MainActivity.java
@@ -2,20 +2,32 @@ package com.ghataa.firebasechatapp;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.firebase.ui.database.FirebaseListOptions;
+import com.ghataa.firebasechatapp.model.ChatMessage;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int SIGN_IN_REQUEST_CODE = 1212;
+
+    private FirebaseListAdapter<ChatMessage> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +52,27 @@ public class MainActivity extends AppCompatActivity {
 
             displayChatMessages();
         }
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText input = findViewById(R.id.input);
+
+                // Read the input field and push a new instance
+                // of ChatMessage to the Firebase database
+                FirebaseDatabase.getInstance()
+                        .getReference()
+                        .push()
+                        .setValue(new ChatMessage(input.getText().toString(),
+                                FirebaseAuth.getInstance().getCurrentUser().getDisplayName())
+                        );
+
+                // Clear the input
+                input.setText("");
+            }
+        });
     }
 
     @Override
@@ -67,7 +100,30 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void displayChatMessages() {
+        ListView listOfMessages = findViewById(R.id.list_of_messages);
 
+        FirebaseListOptions<ChatMessage> options = new FirebaseListOptions.Builder<ChatMessage>()
+                .setLayout(R.layout.message)
+                .setQuery(FirebaseDatabase.getInstance().getReference(), ChatMessage.class)
+                .setLifecycleOwner(this)
+                .build();
+
+        adapter = new FirebaseListAdapter<ChatMessage>(options) {
+            @Override
+            protected void populateView(View view, ChatMessage model, int position) {
+                TextView messageText = view.findViewById(R.id.message_text);
+                TextView messageUser = view.findViewById(R.id.message_user);
+                TextView messageTime = view.findViewById(R.id.message_time);
+
+                messageText.setText(model.getMessageText());
+                messageUser.setText(model.getMessageUser());
+
+                messageTime.setText(DateFormat.format("dd-MM-yyyy (HH:mm:ss)",
+                        model.getMessageTime()));
+            }
+        };
+
+        listOfMessages.setAdapter(adapter);
     }
 
     @Override
